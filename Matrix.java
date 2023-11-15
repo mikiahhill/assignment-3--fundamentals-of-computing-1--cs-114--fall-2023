30a86d8be87e8aa2c9c0994d0ad5e8bd3d3a4841
mikiahhill <mikhill@hartford.edu>
2023-11-15 18:52:56 -0500

    declares variables matrix and size

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,12 @@
 public class Matrix {
+  private int[][]matrix;
+  private int size;
+
+  //Constructor: accepts size as an interger; assigns value to the previously
+  //- declared variables, returns the size of the matrix to user
+  public Matrix(int size){
+    set.size = size;
+    set.matrix = new int[size][size];
+    System.out.println("Your matrix is "+ size + "x" + size);
+  }
 }
