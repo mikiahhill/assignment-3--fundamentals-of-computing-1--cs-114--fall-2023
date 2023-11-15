47a4de974897505fdbac0087bfe8435c9de961af
mikiahhill <mikhill@hartford.edu>
2023-11-15 18:59:01 -0500

    performs swap of given indices

--- Matrix.java
+++ Matrix.java
@@ -9,4 +9,13 @@ public class Matrix {
     set.matrix = new int[size][size];
     System.out.println("Your matrix is "+ size + "x" + size);
   }
+  // reorder variable takes the index of an element in the array and reassigns
+  //the previous element with the element found at the second index
+  private void swap(int x1,int y1 ,int x2,int y2) {
+    int reorder = matrix[x1][y1];
+    matrix[x1][y2] = matrix[x2][y2];
+    matrix[x2][y2]= reorder;
+  }
+
+
 }
