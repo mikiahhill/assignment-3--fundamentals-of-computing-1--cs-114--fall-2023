f3a120a0b7e23f95b3ee5d7f55539185a6f6edf2
mikiahhill <mikhill@hartford.edu>
2023-11-16 00:07:02 -0500

    errors resolved

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,29 +2,30 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
-    int size;
+    int size, x1,x2,y1,y2;//declaring variable
     Scanner scan = new Scanner(System.in);
 
     System.out.println("Please enter the size of your matrix"
     +"(Must be positive: )");
-    size = scan.nextInt();
+    size = scan.nextInt();//store user input in the variable size
 
     if(size<1){
       System.out.println("Matrix cannot be found. Try again!");
     }else{
-    Matrix matrix = new Matrix(size);
-
+    Matrix matrix = new Matrix(size);//creating object matrix of class Matrix
+      //
     System.out.println("Printing matrix with default values: ");
-    matrix.printMatrix();
+    matrix.printMatrix();// invoke the method of printMatrix
+    matrix.populateMatrix();
 
     System.out.println("Populating matrix... matrix populated");
     System.out.println("Printing matrix:");
-    matrix.populateMatrix();
     matrix.printMatrix();
+    matrix.flipMatrix();//Invoke the flipMatrix
 
     System.out.println("\nFlipping matrix...matrix flipped");
     System.out.println("Printing flipped matrix");
-    matrix.flipMatrix();
+    matrix.flipMatrix();//Invoke the flipMatrix
     matrix.printMatrix();
     }
 
